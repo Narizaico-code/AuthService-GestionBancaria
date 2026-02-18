13fb8dd3daaab4e2e352ffd1b573ef72b4b294c1
Narizaico-code <areyes-2024435@kinal.edu.gt>
2026-02-18 16:06:31 -0600

    5. Arreglo de rutas, creacion de administrador default y /all en users.routes

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import { initServer } from './configs/app.js';
 import { sequelize } from './configs/db.js';
 import './src/users/user.model.js';
 import './src/auth/role.model.js';
+import { seedData } from './helpers/data-seeder.js';
 
 // Configurar variables de entorno
 dotenv.config();
+
+await seedData();
 
 await sequelize.authenticate();
 await sequelize.sync({ alter: true });
